68a5a73e7b64eefc72742647fc0fb2cf52ad0f2c
huynhthithdung <169067984+huynhthithdung@users.noreply.github.com>
2024-11-11 21:46:57 +0700

    no merge

--- backend/LibraryManagementAPI/LibraryManagementAPI/obj/Debug/net8.0/LibraryManagementAPI.AssemblyInfo.cs
+++ backend/LibraryManagementAPI/LibraryManagementAPI/obj/Debug/net8.0/LibraryManagementAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibraryManagementAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1ed934743e70169fb40ad47ae81d77a795f829d")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibraryManagementAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibraryManagementAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
