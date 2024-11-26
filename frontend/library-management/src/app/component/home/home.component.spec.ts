4dec9c1d07218bdb9a7cdae4cf451e7f0bd19e47
LeThiThanhTuyen1 <130636825+LeThiThanhTuyen1@users.noreply.github.com>
2024-11-26 14:16:50 +0700

    Revert "Reapply "frontend (decentralization header)""
    This reverts commit f554dd737e46c22a3c49fee84a8006332a70b6b1.

--- frontend/library-management/src/app/component/home/home.component.spec.ts
+++ frontend/library-management/src/app/component/home/home.component.spec.ts
@@ -2,7 +2,6 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FooterComponent } from '../backet/footer/footer.component';
 import { HomeComponent } from './home.component';
 import { HeaderComponent } from '../backet/header/header.component';
-import { LinkLibraryComponent } from '../backet/link-library/link-library.component';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
@@ -12,7 +11,6 @@ describe('HomeComponent', () => {
     await TestBed.configureTestingModule({
       declarations: [
         HomeComponent, 
-        LinkLibraryComponent,
         HeaderComponent,
         FooterComponent]
     })
