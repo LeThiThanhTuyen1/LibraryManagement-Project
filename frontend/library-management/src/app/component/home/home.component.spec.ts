884b4996c325919ee144fa9eba8f8ae099e3fa87
LeThiThanhTuyen1 <130636825+LeThiThanhTuyen1@users.noreply.github.com>
2024-11-26 14:11:00 +0700

    frontend (decentralization header)

--- frontend/library-management/src/app/component/home/home.component.spec.ts
+++ frontend/library-management/src/app/component/home/home.component.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FooterComponent } from '../backet/footer/footer.component';
 import { HomeComponent } from './home.component';
 import { HeaderComponent } from '../backet/header/header.component';
+import { LinkLibraryComponent } from '../backet/link-library/link-library.component';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
@@ -11,6 +12,7 @@ describe('HomeComponent', () => {
     await TestBed.configureTestingModule({
       declarations: [
         HomeComponent, 
+        LinkLibraryComponent,
         HeaderComponent,
         FooterComponent]
     })
